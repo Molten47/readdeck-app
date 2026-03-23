50619d9819090928fa81f7980b89a28bd7acad53
Alawiye Sodeeq <alawiyesodeq@gmail.com>
2026-03-24 00:51:46 +0100

    cahnges made to bestsellsers.ts

--- src/components/landing/BestSellers/useBestSellers.ts
+++ src/components/landing/BestSellers/useBestSellers.ts
@@ -57,7 +57,8 @@ export const useBestSellers = () => {
   useEffect(() => {
     const fetchBooks = async () => {
       try {
-        const res = await fetch('http://localhost:3000/books', {
+        const API = import.meta.env.VITE_API_URL ?? 'http://localhost:3000';
+        const res = await fetch(`${API}/books`, {
           method: 'GET',
           headers: { 'Content-Type': 'application/json' },
         });
