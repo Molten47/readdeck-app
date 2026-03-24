3989f59018d35e2bdb96aaab8a6f0cffcdb3a879
Alawiye Sodeeq <alawiyesodeq@gmail.com>
2026-03-24 01:26:33 +0100

    this does not really makes sense lol

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import DashboardHero from '../components/dashboard/Navbar/components/dashboard/DashboardHero';
-import LandingShell from './LandingShell';
+
 
 const DashboardPage: React.FC = () => (
   <>
     
     <DashboardHero />
-    <LandingShell />
+    
   </>
 );
 
